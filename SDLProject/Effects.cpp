465c0d632ee68edb14dd8234bdc4c5668bd74fdb
Karan R Ganta <karanganta@MacBook-Pro-7.home>
2020-05-09 20:27:16 -0400

    everything I need from class - first edition

---
+++ SDLProject/Effects.cpp
@@ -0,0 +1,9 @@
+//
+//  Effects.cpp
+//  SDLProject
+//
+//  Created by Karan R Ganta on 4/29/20.
+//  Copyright © 2020 ctg. All rights reserved.
+//
+
+#include "Effects.hpp"
---
+++ SDLProject/Effects.h
@@ -0,0 +1,14 @@
+//
+//  Effects.hpp
+//  SDLProject
+//
+//  Created by Karan R Ganta on 4/29/20.
+//  Copyright © 2020 ctg. All rights reserved.
+//
+
+#ifndef Effects_hpp
+#define Effects_hpp
+
+#include <stdio.h>
+
+#endif /* Effects_hpp */
